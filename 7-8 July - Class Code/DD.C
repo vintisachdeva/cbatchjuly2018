793ec9475c3c4c3ed75f25a76f0b0ac17afd6177
vintisachdeva <vintisachdeva17@gmail.com>
2018-07-08 09:16:54 +0530

    Add files via upload

---
+++ 7-8 July - Class Code/DD.C
@@ -0,0 +1,15 @@
+
+   //
+// conio.h --> con -->console window i--> input --> o --> output
+// printf()
+
+void main(){    		//() -->predefined function
+
+	clrscr();	//console clear screen
+	printf("Hello C");	//statement terminate Hello C
+
+	getch();
+}	// }
+
+//
+// Turbo C++ --> by default --> c file --> header file --> stdio.h and conio.h
---
+++ 7-8 July - Class Code/VAR1.C
@@ -0,0 +1,20 @@
+/* program to see an example of variable declartion
+   this is
+*/
+#include<stdio.h>
+#include<conio.h>
+void main(){  // defining this function here
+	// datatype variable_name(identifier) = value;
+	   int a;// garbage value
+	   int b;// garbage value
+	   int c = a + b;  // 10 + 20
+
+	   clrscr(); // it will appear only after variable declaration
+	   //printf(format, value);
+	   // "%d","%c", ""...
+	   // printf(c);
+	   printf("%d", c); // c -->format type --> %d
+	   // ALT + F9
+	   getch();
+
+}
